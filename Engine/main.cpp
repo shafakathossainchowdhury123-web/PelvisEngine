73644233c4f34d94323b0cec26ab7041bf454d70
Shafakat Hossain Chowdhury <shafakathossainchowdhury123@gmail.com>
2026-08-08 15:10:35 +0000

    Pelvis Engine 0.1 - initial CMake engine

---
+++ Engine/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "====================================\n";
+    std::cout << "        PELVIS ENGINE 0.1\n";
+    std::cout << "====================================\n";
+    std::cout << "Engine started successfully!\n";
+    std::cout << "Cloud build environment is working.\n";
+    std::cout << "====================================\n";
+
+    return 0;
+}
---
+++ build/CMakeFiles/PelvisEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PelvisEngine.dir/Engine/main.cpp.o"
+  "CMakeFiles/PelvisEngine.dir/Engine/main.cpp.o.d"
+  "PelvisEngine"
+  "PelvisEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PelvisEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
